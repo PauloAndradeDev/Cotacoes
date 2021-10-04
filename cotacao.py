c87aa347fb2b6d22c134f5b7e5fadce40248a490
Andrade <luigipaulo.desenvolvedor@gmail.com>
2021-10-04 00:44:46 -0300

    Título da janela principal definido

--- cotacao.py
+++ cotacao.py
@@ -1,2 +1,6 @@
 from cotacoes_moedas.acessando_cotacoes import formatar_saida
+from graph.main_window import MainWindow
 
+cotacoes = formatar_saida()
+window = MainWindow(cotacoes[0], cotacoes[1], cotacoes[2])
+window.run()
--- graph/main_window.py
+++ graph/main_window.py
@@ -6,11 +6,13 @@ from tkinter.ttk import Separator
 
 
 class MainWindow:
-    def __init__(self) -> None:
+    def __init__(self, dolar: str, euro: str, bitcoin: str) -> None:
         """Construtor da classe MainWindow."""
+        self.__lista_cotacoes: list = list([dolar, euro, bitcoin])
         self.__main_window: Tk = Tk()
-        self.__main_window.title("Principais Cotações")
+        self.__main_window.title("Cotações")
         self.__main_window.configure(background="#FFFFFF")
+        self.__main_window.resizable(0, 0)
         self.__main_window.geometry(
             newGeometry="%dx%d+%d+%d" % (
                 400,
@@ -20,12 +22,17 @@ class MainWindow:
             )
         )
         # Make Widgets:
-        self.__make_labels()
+        self.__make_labels(self.__lista_cotacoes[0], self.__lista_cotacoes[1], self.__lista_cotacoes[2])
         self.__make_separators()
 
     def __make_labels(self, dolar: str, euro: str, bitcoin: str) -> None:
         """Método responsável por criar todas as labels da janela principal."""
-        pass
+        Label(
+            master=self.__main_window, text="Cotações do Dia", background="#FFFFFF", font=("Candara Light Italic", 20)
+        ).place(x=100, y=5)
+        # Label(master=self.__main_window, text="", background="").place(x=, y=)
+        # Label(master=self.__main_window, text="", background="").place(x=, y=)
+        # Label(master=self.__main_window, text="", background="").place(x=, y=)
 
     def __make_separators(self) -> None:
         """Método responsável por criar todos os separadores da janela principal."""
