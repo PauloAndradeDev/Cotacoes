d53d7e2026419b267da8c021fcdf789261013e51
Andrade <luigipaulo.desenvolvedor@gmail.com>
2021-10-04 01:35:35 -0300

    labels e separators concluídos

--- cotacao.py
+++ cotacao.py
@@ -2,5 +2,14 @@ from cotacoes_moedas.acessando_cotacoes import formatar_saida
 from graph.main_window import MainWindow
 
 cotacoes = formatar_saida()
-window = MainWindow(cotacoes[0], cotacoes[1], cotacoes[2])
+
+dolar = round(float(cotacoes[0]), 2)
+euro = round(float(cotacoes[1]), 2)
+bitcoin = round(float(cotacoes[2]), 3)
+
+dolar = str(dolar).replace(".", ",")
+euro = str(euro).replace(".", ",")
+bitcoin = str(bitcoin)
+
+window = MainWindow(dolar, euro, bitcoin)
 window.run()
--- graph/main_window.py
+++ graph/main_window.py
@@ -28,19 +28,40 @@ class MainWindow:
     def __make_labels(self, dolar: str, euro: str, bitcoin: str) -> None:
         """Método responsável por criar todas as labels da janela principal."""
         Label(
-            master=self.__main_window, text="Cotações do Dia", background="#FFFFFF", font=("Candara Light Italic", 20)
-        ).place(x=100, y=5)
-        # Label(master=self.__main_window, text="", background="").place(x=, y=)
-        # Label(master=self.__main_window, text="", background="").place(x=, y=)
-        # Label(master=self.__main_window, text="", background="").place(x=, y=)
+            master=self.__main_window,
+            text="Cotações do Dia",
+            background="#FFFFFF",
+            foreground="#808080",
+            font=("Candara Light Italic", 20)
+        ).place(x=110, y=5)
+        Label(master=self.__main_window, text="DOLAR", background="#FFFFFF", foreground="#436253", font=("Arial", 13)).place(x=38, y=60)
+        Label(master=self.__main_window, text="EURO", background="#FFFFFF", foreground="#3e4758", font=("Arial", 13)).place(x=167, y=60)
+        Label(master=self.__main_window, text="BITCOIN", background="#FFFFFF", foreground="#8ea97c", font=("Arial", 13)).place(x=280, y=60)
+        Label(master=self.__main_window,
+              text=f"R$ {dolar}",
+              background="#FFFFFF",
+              foreground="#436253",
+              font=("Arial Black", 14)
+              ).place(x=25, y=85)
+        Label(master=self.__main_window,
+              text=f"R$ {euro}",
+              background="#FFFFFF",
+              foreground="#3e4758",
+              font=("Arial Black", 14)
+              ).place(x=148, y=85)
+        Label(master=self.__main_window,
+              text=f"R$ {bitcoin}",
+              background="#FFFFFF",
+              foreground="#8ea97c",
+              font=("Arial Black", 14)
+              ).place(x=260, y=85)
+
+    def __make_buttons(self):
+        pass
 
     def __make_separators(self) -> None:
         """Método responsável por criar todos os separadores da janela principal."""
-        pass
+        Separator(master=self.__main_window, orient="horizontal").place(x=25, y=40, width=350)
 
     def run(self) -> None:
         self.__main_window.mainloop()
-
-
-if __name__ == "__main__":
-    MainWindow().run()
